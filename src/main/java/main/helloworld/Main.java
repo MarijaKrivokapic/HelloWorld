291c71e3be8da0b79066d0eeeb40031d16d9b7b2
Marija Krivokapic <marija.p.krivokapic@gmail.com>
2020-02-04 20:20:36 +0100

    Adds age checker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,7 @@
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -13,6 +15,46 @@ public class Main {
         
         System.out.println("Hello World");
         System.out.println ("Marija Krivokapic");
-    }
+        
+        int age = 5;
+        
+        if (age >= 18) {
+            System.out.println("Osoba je punoletna.");
+    } else {
+            System.out.println("Osoba je maloletna.");
+        }
+        
+        System.out.print("Unesite broj godina: ");
+        Scanner input = new Scanner (System.in);
+        int userAge= input.nextInt();
+        
+        System.out.println("Korisnik je uneo " + userAge);
     
+        if (userAge >= 18) {
+            System.out.print("Osoba je punoletna.");
+    } else {
+            System.out.print("Osoba je maloletna.");
+        }
+      
+        System.out.print("Unesite broj godina osobe: ");
+        input = new Scanner (System.in);
+        int personAge= input.nextInt();
+        
+        if (personAge <= 0 && personAge > 150){
+             System.out.print("Invalid");}
+             else if (personAge > 0 && personAge <= 5){
+             System.out.print("Baby");
+    } else if (personAge <=11){
+            System.out.print("Kid");
+        }else if (personAge <=17){
+        System.out.print("Teen");
+        }else if (personAge >=18){
+                System.out.print("Adult");}
+    
+
+        
+        }
+        
+        
 }
+
